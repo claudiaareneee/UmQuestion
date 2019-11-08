ac477065f1b735ad8eaa7c314d216a2577881aa9
Claudia <claudiaareneee@gmail.com>
2019-11-08 10:51:50 -0600

    added login page

---
+++ public/login.js
@@ -0,0 +1,17 @@
+var email = document.getElementById("email");
+var password = document.getElementById("password");
+
+var submitButton = document.getElementById("loginButton");
+submitButton.onclick = function(){
+    //TODO: verify user
+
+    if(email.value != "" && password.value != "")
+        window.location = "course.html";
+    else
+        alert("please fill in fields");
+};
+
+var loginButton = document.getElementById("newUserButton");
+loginButton.onclick = function(){
+    window.location = "newUser.html";
+};
