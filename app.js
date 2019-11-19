4aac16a3604e9878752539fbeb6aa40b75d7f0e0
Carlos Martinez <carlos_martinez227@yahoo.com>
2019-11-19 17:09:54 -0600

    none

--- app.js
+++ app.js
@@ -178,8 +178,8 @@ io.on('connection', function(socket){
 	});
 
 	socket.on('fetch_single_course', async function(dta){
-		let res1 = await client.query("SELECT * FROM course WHERE cid="+ dta);
-		socket.emit('received_single_course', res1.rows[0].name);
+			let res1 = await client.query("SELECT * FROM course WHERE cid="+ dta);
+			socket.emit('received_single_course', res1.rows[0].name);
 	});
 
 	socket.on('delete_course', async function(dta){
